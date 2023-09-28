0d71ed2024cae7195757df2c5ef003227d6300e7
kadunapikin <ibakoshi27@gmail.com>
2023-09-28 16:52:46 +0100

    define a simple schema and model for our todo

--- server/server.js
+++ server/server.js
@@ -10,3 +10,15 @@ mongoose.connect('mongodb://localhost:27017/todoApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define a simple schema and model for our Todo
+const TodoSchema = new mongoose.Schema({
+    task: String,
+    completed: Boolean,
+  });
+  
+  const Todo = mongoose.model('Todo', TodoSchema);
