5fe5401f2df57a82e6c80e59dbf8b4775efbb221
kadunapikin <ibakoshi27@gmail.com>
2023-09-30 00:25:25 +0100

    create handleChange function on Create.js

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+    const [task, setTask] = useState('');
+    const handleChange = (e) => {
+        setTask((e.target.value))
+    }
   return (
     <div className='create_form'>
-        <input type='text' name='' id='' placeholder='Enter A Task' />
+        <input type='text' name='' id='' placeholder='Enter A Task' onChange={handleChange} />
         <button type='button'>Add Todo</button>
     </div>
   )
