529411ac3ab097c5251615a5efab5520d98cf100
kadunapikin <ibakoshi27@gmail.com>
2023-09-30 03:01:01 +0100

    create handleAdd function to add task on Create.js

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const Create = () => {
     const [task, setTask] = useState('');
+
     const handleChange = (e) => {
         setTask((e.target.value))
+    }
+
+    const handleAdd = () => {
+        axios.post('http://localhost:3001/add', {task: task})
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
     }
   return (
     <div className='create_form'>
         <input type='text' name='' id='' placeholder='Enter A Task' onChange={handleChange} />
-        <button type='button'>Add Todo</button>
+        <button onClick={handleAdd} type='button'>Add Todo</button>
     </div>
   )
 }
