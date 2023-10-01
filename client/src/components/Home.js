5a4be3680005e4358f6ed19a68007270fcad252c
kadunapikin <ibakoshi27@gmail.com>
2023-10-01 22:56:02 +0100

    add checkbox and delete icon to Home.js component and style the icons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
 import Create from './Create'
 import "./Home.css"
 
 const Home = () => {
 const [todos, setTodos] = useState([]);
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 useEffect(() => {
   axios.get('http://localhost:3001/get').then(result => setTodos(result.data)).catch(err => console.log(err));
 }, [])
@@ -16,9 +19,17 @@ useEffect(() => {
         {
             todos.length === 0 ? <div><h3>No Record of any todo task!</h3></div>:
             todos.map(todo => {
-                return (
-                    <div className='task'>{todo.task}</div>
-                )
+              return (
+                <div className='task'>
+                <div className='checkbox'>
+                <Checkbox {...label} className='icon' />
+                  <p>{todo.task}</p>
+                </div>
+                <div>
+                  <span><DeleteIcon className='icon' /></span>
+                </div>
+              </div>
+              )
             })
         }
     </div>
