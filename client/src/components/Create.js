3205b5746ba303d2c9d35d54f9c6000e0671c5a0
kadunapikin <ibakoshi27@gmail.com>
2023-10-02 16:05:53 +0100

    add updateTodoList on Home.js and pass it as props on Create.js to update todo list

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 
-const Create = () => {
+const Create = ({ updateTodoList }) => {
     const [task, setTask] = useState('');
 
     const handleChange = (e) => {
@@ -9,12 +9,20 @@ const Create = () => {
     }
 
     const handleAdd = () => {
-        axios.post('http://localhost:3001/add', {task: task})
+      if (!task.trim()) {
+        alert('Please enter a new task to add!!!'); 
+        return; 
+      }
+      axios.post('http://localhost:3001/add', { task: task })
         .then(result => {
-          window.location.reload();
+          // Call the updateTodoList function passed from Home.js
+          setTask('');
+          updateTodoList(result.data);
+           // Clear the input field
         })
         .catch(err => console.log(err));
-    }
+    };
+
   return (
     <div className='create_form'>
         <input type='text' name='' id='' placeholder='Enter A Task' onChange={handleChange} />
