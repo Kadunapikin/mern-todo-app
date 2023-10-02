9ff59f884ee64cef3590b24613086e14012fe976
kadunapikin <ibakoshi27@gmail.com>
2023-10-02 16:29:51 +0100

    imput useRef to Create.js to clear input field after updating todo list

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,32 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 const Create = ({ updateTodoList }) => {
-    const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
+  const inputRef = useRef(null);
 
-    const handleChange = (e) => {
-        setTask((e.target.value))
+
+  const handleChange = (e) => {
+      setTask((e.target.value))
+  }
+
+  const handleAdd = () => {
+    if (!task.trim()) {
+      alert('Please enter a new task to add!!!');
+      return;
     }
-
-    const handleAdd = () => {
-      if (!task.trim()) {
-        alert('Please enter a new task to add!!!'); 
-        return; 
-      }
-      axios.post('http://localhost:3001/add', { task: task })
-        .then(result => {
-          // Call the updateTodoList function passed from Home.js
-          setTask('');
-          updateTodoList(result.data);
-           // Clear the input field
-        })
-        .catch(err => console.log(err));
-    };
-
+  
+  axios.post('http://localhost:3001/add', { task: task })
+    .then(result => {
+      // Clear the input field using the ref
+      inputRef.current.value = '';
+      setTask(''); // Clear the state as well
+      updateTodoList(result.data);
+    })
+      .catch(err => console.log(err));
+  };
+    
   return (
     <div className='create_form'>
-        <input type='text' name='' id='' placeholder='Enter A Task' onChange={handleChange} />
-        <button onClick={handleAdd} type='button'>Add Todo</button>
+      <input
+        type='text'
+        name=''
+        id=''
+        placeholder='Enter A Task'
+        onChange={handleChange}
+        ref={inputRef} // Attach the ref to the input element
+      />
+      <button onClick={handleAdd} type='button'>Add Todo</button>
     </div>
   )
 }
